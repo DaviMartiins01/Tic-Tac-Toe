21375fe71b7f9842c6e1363edf8d4fd5ab235300
Davi Martins <davibarroso12399@gmail.com>
2020-12-09 14:04:46 -0300

    Function PrintValue

--- tictactoe.c
+++ tictactoe.c
@@ -2,20 +2,13 @@
 #include <stdio.h>
 
 void Draw(void);
+void PrintValue();
 
 char coordinates[3][3];
 
 int main()
 {
-    coordinates[0][0] = ' ';
-    coordinates[0][1] = ' ';
-    coordinates[0][2] = ' ';
-    coordinates[1][0] = ' ';
-    coordinates[1][1] = ' ';
-    coordinates[1][2] = ' ';
-    coordinates[2][0] = ' ';
-    coordinates[2][1] = ' ';
-    coordinates[2][2] = ' ';
+    PrintValue();
     Draw();
     return 0;
 }
@@ -31,5 +24,15 @@ void Draw(void)
     printf("       |       |       \n");
     printf("   %c   |   %c   |   %c   \n", coordinates[2][1], coordinates[2][1], coordinates[2][2]);
     printf("       |       |       \n");
+}
 
+void PrintValue()
+{
+    for(int i = 0; i < 3; i++)
+    {
+       for(int e = 0; e < 3; e++)
+       {
+          coordinates[i][e] = ' ';
+       }
+    }
 }
